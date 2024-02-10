d20b517fb30a3f6397335f41b22b7bd223d388a2
Arun kumar <145600109+candy-arun@users.noreply.github.com>
2024-02-10 21:08:50 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,18 +1,14 @@
-import java.sql.SQLOutput;
 import java.util.Scanner;
+// 'X' or 'x' to Exit the calculator
 
 public class Calculator {
     public static void main(String[] args) {
         Scanner in =  new Scanner(System.in);
+        // opeaning a infinity  for loop 
         while (true){
             int ans = 0;
             System.out.print("Enter the operator :");
             char op = in.next().trim().charAt(0);
-//            System.out.print("Enter the number1 :");
-//            int n1 = in.nextInt();
-//            System.out.print("Enter the number2 :");
-//            int n2 = in.nextInt();
-
             if(op == '+' || op =='-' || op == '%' || op== '*' || op =='/'){
                 System.out.print("Enter the number1 :");
                 int n1 = in.nextInt();
@@ -29,17 +25,14 @@ public class Calculator {
                 }
                 if(op == '/' && n2 >0){
                     ans = n1/n2;
-
                 }
                 if(op =='%'){
                     ans = n1%n2;
                 }
             }
-
             else if(op == 'X' || op =='x'){
                 break;
             }
-
             else{
                 System.out.println("invalid operator");
             }
